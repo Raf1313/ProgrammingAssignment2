b29e9b93249c13278610d4174a8731c4fc9952cd
Raf <rafael.ritondo@gmail.com>
2017-06-05 16:39:13 +0200

    Work completed

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,39 @@
-## Put comments here that give an overall description of what your
-## functions do
+## this code is breaking in two function. They take a matrix, check if the inverse of this matrix has already
+## been computed and stored. If it's the cas, it return these value, else, it computes it and store it
+## in the global environnement
 
-## Write a short comment describing this function
+
+## Thse function create a list of four functions. One set an object for the original matrix and 
+## inverted one in the global environnement. The second one allows to get the value of the original matrix
+## supplied as argumend, the thirst store the inverted value in the global environnement and the last one
+## get this value.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+i<-NULL
+    set<-function(y){
+        x<<-y
+        i<<-NULL
+  }
+    get<- function() x
+    setinvert<-function(invert) i<<-invert
+    getinvert<-function () i
+    list(set=set, get=get, setinvert=setinvert, getinvert=getinvert)
 }
 
 
-## Write a short comment describing this function
+## These function use the function set in makeCacheMatrix. it checks if the inverted has already
+## computed. If it is the cas, it returns the value, else it computes it and store it in the global 
+## environnement.
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  i<-x$getinvert()
+  if(!is.null(i)){
+    message("getting catched invert")
+    return(i)
+  }
+  data<-x$get()
+  i<-solve(data,...)
+  x$setinvert(i)
+  i
 }
